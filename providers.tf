72d86c4bec087380e6439161518b725de8fd7b0e
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-10 20:28:15 +0300

    update

---
+++ providers.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Use a recent version
+    }
+  }
+  required_version = ">= 1.5.0" # Ensures you have a recent Terraform version
+}
